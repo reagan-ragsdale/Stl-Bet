d57dc45cbf05ea5533300526f8b96fc36cbd11b8
reagan-ragsdale <rayrags10@gmail.com>
2023-10-27 11:50:19 -0500

    sdfg

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,26 +6,16 @@ import { createKnexDataProvider } from "remult/remult-knex"
 import helmet from "helmet"
 import compression from "compression"
 import path from "path"
-import { JsonDataProvider } from "remult"
-import { JsonEntityFileStorage } from "remult/server"
-import { remultExpress } from "remult/remult-express"
 
 const app = express()
-
 app.use(
-  remultExpress({
-  dataProvider: async () =>
-    new JsonDataProvider(new JsonEntityFileStorage("../db"))
-})
-    /* session({
-      secret: process.env["SESSION_SECRET"] || "my secret"
-    }) */
-  )
-//app.use(auth)
+  session({
+    secret: process.env["SESSION_SECRET"] || "my secret"
+  })
+)
 app.use(helmet())
 app.use(compression())
 app.use(api)
-
 app.use(express.static(path.join(__dirname, "../remult-angular-todo")))
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../remult-angular-todo", "index.html"))
