f3fea2f3166e29cb749ff682b3a32a2aa8a66645
reagan-ragsdale <rayrags10@gmail.com>
2023-12-04 17:38:37 -0600

    ds

--- src/server/api.ts
+++ src/server/api.ts
@@ -40,7 +40,7 @@ export const api = remultExpress({
     NbaController,
   ],
   dataProvider: createPostgresDataProvider({
-    connectionString: process.env["postgresql://postgres:eg*gE31aCf66e5A*A5G35*3d3g1fgCcC@viaduct.proxy.rlwy.net:47427/railway"] || "your connection string"
+    connectionString: process.env["postgresql://postgres:eg*gE31aCf66e5A*A5G35*3d3g1fgCcC@postgres.railway.internal:5432/railway"] || "your connection string"
   })
   // getUser: req => req.session!["user"]
 });
