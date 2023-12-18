362866dd83b5a625cc3a19dbff8e1f0ef327eabe
reagan-ragsdale <rayrags10@gmail.com>
2023-12-18 10:58:06 -0600

    cron

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,7 @@ import compression from 'compression';
 import path from 'path';
 import { api } from './api';
 import { createPostgresDataProvider } from 'remult/postgres';
-import { cronTestFile } from '../app/cronTest';
+var cronTestFile = require('../app/cronTest');
 const connectionString = "postgresql://postgres:eg*gE31aCf66e5A*A5G35*3d3g1fgCcC@postgres.railway.internal:5432/railway"
 const app = express();
 
