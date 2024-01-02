c5e32ff0399e06f295ebee577696f0e026260419
reagan-ragsdale <rayrags10@gmail.com>
2024-01-02 11:41:06 -0600

    fdgh

--- src/shared/Controllers/MlbController.ts
+++ src/shared/Controllers/MlbController.ts
@@ -1,5 +1,5 @@
 import { Allow, BackendMethod, remult } from "remult"
-import { PlayerInfoMlb } from "../dbTasks/PlayerInfoMlb.js"
+import { PlayerInfoMlb } from "../dbTasks/PlayerInfoMlb"
 
 export class MlbController {
 
