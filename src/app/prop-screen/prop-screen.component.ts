e6a9c77115d51cb0f909238c6f37188fed86330d
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-01-05 15:37:00 -0600

    df

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -696,7 +696,7 @@ console.log(team1)
     totalPoint = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Over")[0].point.toString();
     totalPrice = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Over")[0].price.toString();
     console.log(team1[0].teamName)
-    teamInfo = await NbaController.nbaGetLogoFromTeamName(name1)
+    teamInfo = await NbaController.nbaGetLogoFromTeamId(15)
     console.log(teamInfo)
     this.displayPropHtml1 = ({ name: name1, h2h: h2h, spreadPoint: spreadPoint, spreadPrice: spreadPrice, totalPoint: totalPoint, totalPrice: totalPrice, primaryColor: teamInfo[0].primaryColor, alternateColor: teamInfo[0].alternateColor });
     console.log(team2)
