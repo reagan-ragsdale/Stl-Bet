363293cc8f0e34ae4f81d1e49a8997457c7ee2d6
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-01-05 16:38:19 -0600

    asd

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -695,20 +695,16 @@ console.log(team1)
     spreadPrice = team1.filter((e) => e.marketKey == "spreads")[0].price.toString();
     totalPoint = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Over")[0].point.toString();
     totalPrice = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Over")[0].price.toString();
-    console.log(team1[0].teamName)
-    teamInfo = await NbaController.nbaGetLogoFromTeamId(15)
-    console.log(teamInfo)
+    teamInfo = await NbaController.nbaGetLogoFromTeamName(name1)
     this.displayPropHtml1 = ({ name: name1, h2h: h2h, spreadPoint: spreadPoint, spreadPrice: spreadPrice, totalPoint: totalPoint, totalPrice: totalPrice, primaryColor: teamInfo[0].primaryColor, alternateColor: teamInfo[0].alternateColor });
-    console.log(team2)
+    
     name1 = team2[0].teamName;
     h2h = team2.filter((e) => e.marketKey == "h2h")[0].price.toString();
     spreadPoint = team2.filter((e) => e.marketKey == "spreads")[0].point.toString();
     spreadPrice = team2.filter((e) => e.marketKey == "spreads")[0].price.toString();
     totalPoint = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Under")[0].point.toString();
     totalPrice = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Under")[0].price.toString();
-    //teamInfo = await NbaController.nbaGetLogoFromTeamName(name1)
-    teamInfo = await NbaController.nbaGetLogoFromTeamId(15)
-    
+    teamInfo = await NbaController.nbaGetLogoFromTeamName(name1)
     this.displayPropHtml2 = ({ name: name1, h2h: h2h, spreadPoint: spreadPoint, spreadPrice: spreadPrice, totalPoint: totalPoint, totalPrice: totalPrice, primaryColor: teamInfo[0].primaryColor, alternateColor: teamInfo[0].alternateColor });
     console.timeEnd("Display Prop")
     this.teamPropIsLoading = false
