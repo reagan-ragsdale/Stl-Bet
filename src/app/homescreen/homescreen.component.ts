e4221d41cfbf348d143c763ef56ed0010452c896
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-01-08 17:08:18 -0600

    sdfg

--- src/app/homescreen/homescreen.component.ts
+++ src/app/homescreen/homescreen.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'home-screen',
   templateUrl: './homescreen.component.html',
-  styleUrls: ['./homescreen.component.scss']
+  styleUrls: ['./homescreen.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeScreenComponent {
   constructor(private router: Router){}
