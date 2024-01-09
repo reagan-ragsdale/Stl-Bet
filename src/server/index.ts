617a062e68c22cdb9df744c5557bd6ce93d642e3
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-01-09 12:33:52 -0600

    asdf

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,7 @@ import compression from 'compression';
 import path from 'path';
 import { api } from './api';
 import { createPostgresDataProvider } from 'remult/postgres';
-import { cronTestFile } from '../app/cronTest';
+//import { cronTestFile } from '../app/cronTest';
 const connectionString = "postgresql://postgres:eg*gE31aCf66e5A*A5G35*3d3g1fgCcC@postgres.railway.internal:5432/railway"
 const app = express();
 import cron from 'node-cron'
@@ -57,4 +57,4 @@ app.get('/*', (req, res) => {
 app.listen(process.env['PORT'] || 3002, () => console.log('Server started'));
 
 
-cron.schedule('* * * * *',  cronTestFile)
+//cron.schedule('* * * * *',  cronTestFile)
