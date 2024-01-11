159bb3b90c05fc3b4a73ab0d28f08986dfef4629
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-01-11 14:18:51 -0600

    asf

--- src/app/cronTest.ts
+++ src/app/cronTest.ts
@@ -55,7 +55,7 @@ export const cronTestFile = async () => {
     var listOfGamesToday: DbGameBookData[] = await SportsBookController.loadSportBook("NBA")
     var listOfFilteredGame
     listOfFilteredGame = listOfGamesToday.filter(e => {
-        console.log(typeof(e.commenceTime))
+        console.log(typeof e.commenceTime)
         //console.log(convertDate(e.commenceTime.toString()) == getMonthAndDay())
         convertDate(e.commenceTime.toString()) == getMonthAndDay()
     })
