73cb0760141553f8c2a856d4d26649cd8bbc9985
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-02-06 10:30:55 -0600

    ad

--- src/server/api.ts
+++ src/server/api.ts
@@ -53,7 +53,7 @@ export const api = remultExpress({
         connectionString: process.env['DATABASE_URL']
       }) : undefined
 ,initApi: async ()=>{
-  cron.schedule('30 10 * * *',()=>  cronTestFile())
+  cron.schedule('37 16 * * *',()=>  cronTestFile())
   //cron.schedule('0 10 * * *', ()=>  mlbCronFile())
 }
 });
