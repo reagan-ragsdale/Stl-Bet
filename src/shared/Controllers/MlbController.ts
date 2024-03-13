c83cccaf737e916e1ab0232f8d452f27aa995fa1
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-03-13 12:57:11 -0500

    df

--- src/shared/Controllers/MlbController.ts
+++ src/shared/Controllers/MlbController.ts
@@ -112,16 +112,16 @@ export class MlbController {
     var finalData: DBMlbPlayerGameStatAverages[] = []
 
     if(stat == "hits"){
-      finalData = await taskRepo.find({orderBy: {batterHits: "desc"}, limit: 5})
+      finalData = await taskRepo.find({where: {totalGames:{">": 60} }, orderBy: {batterHits: "desc"}, limit: 5})
     }
     else if(stat == "homeRuns"){
-      finalData = await taskRepo.find({orderBy: {batterHomeRuns: "desc"}, limit: 5})
+      finalData = await taskRepo.find({where: {totalGames:{">": 60} },orderBy: {batterHomeRuns: "desc"}, limit: 5})
     }
     else if(stat == "rbis"){
-      finalData = await taskRepo.find({orderBy: {batterRbis: "desc"}, limit: 5})
+      finalData = await taskRepo.find({where: {totalGames:{">": 60} },orderBy: {batterRbis: "desc"}, limit: 5})
     }
     else if(stat == "pitcherStrikes"){
-      finalData = await taskRepo.find({orderBy: {pitcherStrikes: "desc"}, limit: 5})
+      finalData = await taskRepo.find({where: {totalGames:{">": 60} },orderBy: {pitcherStrikes: "desc"}, limit: 5})
     }
     
     return finalData
