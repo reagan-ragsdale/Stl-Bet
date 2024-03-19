1a92336c7256d01be08b691175edf01d7ade6d15
reagan-ragsdale <74213557+reagan-ragsdale@users.noreply.github.com>
2024-03-19 13:54:21 -0500

    df

--- src/app/welcome-screen/welcome-screen.component.ts
+++ src/app/welcome-screen/welcome-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-screen',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-screen.component.sass']
 })
 export class WelcomeScreenComponent {
+  constructor(private router: Router) { 
+  }
+ 
 
 }
