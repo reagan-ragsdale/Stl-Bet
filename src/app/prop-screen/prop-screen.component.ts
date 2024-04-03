37b5840c3e120a89c85e3454b607963f9f510d1e
Reagan Ragsdale <rayrags10@gmail.com>
2024-04-03 13:22:30 -0500

    asd

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -856,7 +856,7 @@ console.log(team1)
     totalPoint = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Over")[0].point.toString();
     totalPrice = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Over")[0].price.toString();
     this.displayPropHtml1 = ({ name: name1, abvr: reusedFunctions.addDash(name1), h2h: h2h, spreadPoint: spreadPoint, spreadPrice: spreadPrice, totalPoint: totalPoint, totalPrice: totalPrice});
-
+console.log(this.displayPropHtml1)
     name1 = team2[0].teamName;
     h2h = team2.filter((e) => e.marketKey == "h2h")[0].price.toString();
     spreadPoint = team2.filter((e) => e.marketKey == "spreads")[0].point.toString();
@@ -864,7 +864,7 @@ console.log(team1)
     totalPoint = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Under")[0].point.toString();
     totalPrice = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Under")[0].price.toString();
     this.displayPropHtml2 = ({ name: name1, abvr: reusedFunctions.addDash(name1), h2h: h2h, spreadPoint: spreadPoint, spreadPrice: spreadPrice, totalPoint: totalPoint, totalPrice: totalPrice });
-    console.timeEnd("Display Prop")
+    console.log(this.displayPropHtml2)
     this.teamPropIsLoading = false
   }
 
