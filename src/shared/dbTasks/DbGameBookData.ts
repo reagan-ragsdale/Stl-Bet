7600117c72c095492b445ccba82e251c217ce641
Reagan Ragsdale <rayrags10@gmail.com>
2024-04-02 22:22:56 -0500

    sd

--- src/shared/dbTasks/DbGameBookData.ts
+++ src/shared/dbTasks/DbGameBookData.ts
@@ -66,7 +66,7 @@ export class DbGameBookData {
   static allSportFilterByMaxBookSeqAndh2h = Filter.createCustom<DbGameBookData, { sport: string }>(async ({sport}) => {
     SqlDatabase.LogToConsole = true
     return SqlDatabase.rawFilter((whereFragment) => {
-      whereFragment.sql = 'bookSeq = (select max(b.bookSeq) from DbGameBookData b where b.sportTitle = ' + whereFragment.addParameterAndReturnSqlToken(sport) + ' and b.bookId = bookId and date(commencetime) >= CURRENT_DATE) and sportTitle = ' + whereFragment.addParameterAndReturnSqlToken(sport) + ' and date(commencetime) >= CURRENT_DATE and marketkey = ' + 'h2h'
+      whereFragment.sql = 'bookSeq = (select max(b.bookSeq) from DbGameBookData b where b.sportTitle = ' + whereFragment.addParameterAndReturnSqlToken(sport) + ' and b.bookId = bookId and date(commencetime) >= CURRENT_DATE) and sportTitle = ' + whereFragment.addParameterAndReturnSqlToken(sport) + ' and date(commencetime) >= CURRENT_DATE and marketkey = ' + whereFragment.addParameterAndReturnSqlToken('h2h')
     })
     
     
