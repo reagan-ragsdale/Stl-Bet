c009837cb745eb904ca99ad09ef06f60aeec75fb
Reagan Ragsdale <rayrags10@gmail.com>
2024-04-15 10:21:03 -0500

    bn

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -1398,7 +1398,7 @@ export class PropScreenComponent implements OnInit {
     totalPoint = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Under")[0].point;
     totalPrice = tempProp.filter((e) => e.marketKey == "totals" && e.teamName == "Under")[0].price;
     this.displayPropHtml2 = ({ name: name1, abvr: abvr, h2h: h2h, spreadPoint: spreadPoint, spreadPrice: spreadPrice, totalPoint: totalPoint, totalPrice: totalPrice, commenceTime: spreadPriceProp[0].commenceTime });
-
+    this.team2SelectedSpreadPoint = spreadPoint
     
     this.teamPropIsLoading = false
   }
