f82dfe4fc927c56afad7ad3fde2b19afc6880045
Reagan Ragsdale <rayrags10@gmail.com>
2024-04-22 15:09:05 -0500

    jh

--- src/app/cronJobs/cronSportsBookLoadHourly.ts
+++ src/app/cronJobs/cronSportsBookLoadHourly.ts
@@ -4,7 +4,7 @@ import { draftKingsApiController } from "../ApiCalls/draftKingsApiCalls";
 export const cronSportsBookHourly = async () => {
 
 
-    const listOfActiveSports: string[] = ["MLB", "NBA"]
+    const listOfActiveSports: string[] = ["MLB"]
 
     const listOfMlbExtraGameProps: string = "alternate_spreads,alternate_totals"
 
