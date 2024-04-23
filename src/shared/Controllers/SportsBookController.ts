8adf19809bd0363a0e477fc05aad1e159dea64f7
Reagan Ragsdale <rayrags10@gmail.com>
2024-04-23 11:36:20 -0500

    h

--- src/shared/Controllers/SportsBookController.ts
+++ src/shared/Controllers/SportsBookController.ts
@@ -33,7 +33,7 @@ export class SportsBookController {
             if(filteredNewProp.length != 0){
               console.log("filteredNewProp below")
               console.log(filteredNewProp[0])
-              await taskRepo.save({...prop, createdAt: filteredNewProp[0].createdAt, price: filteredNewProp[0].price, point: filteredNewProp[0].point})
+              await taskRepo.save({...prop, price: filteredNewProp[0].price, point: filteredNewProp[0].point, bookMaker: 'Saved'})
             }
           }
         }
