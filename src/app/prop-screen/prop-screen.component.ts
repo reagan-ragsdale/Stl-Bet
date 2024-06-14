37ffe1c03ebbc3f85cb99ecd899270aa36e3e105
Reagan Ragsdale <rayrags10@gmail.com>
2024-06-14 11:02:51 -0500

    nm

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -3203,7 +3203,7 @@ export class PropScreenComponent implements OnInit {
           
           returnProp = this.getTeamStats(prop[0], team[0][0].teamName)
         }
-        prop.propVariables = returnProp
+        prop[0].propVariables = returnProp
       }
     }
   }
