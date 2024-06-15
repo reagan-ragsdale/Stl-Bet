c445e5483d762406871872a537fc6bf2b2364f33
Reagan Ragsdale <rayrags10@gmail.com>
2024-06-15 18:43:23 -0500

    asd

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -69,6 +69,7 @@ import { DbPlayerInfo } from '../../shared/dbTasks/DbPlayerInfo';
 
 export class PropScreenComponent implements OnInit {
 
+
   @HostListener('window:popstate', ['$event'])
   onPopState(event: any) {
     location.reload()
