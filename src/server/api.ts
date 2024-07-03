992f67c8ff25ccdd4acdf432bb2a9c4e619d71ce
Reagan Ragsdale <rayrags10@gmail.com>
2024-07-03 12:12:50 -0500

    fhg

--- src/server/api.ts
+++ src/server/api.ts
@@ -73,7 +73,7 @@ export const api = remultExpress({
     //cron.schedule('30 15 * * *', () => cronTestFile())
     //1:33pm
 
-    cron.schedule('10 17 * * *', () => mlbCronFile())
+    cron.schedule('22 17 * * *', () => mlbCronFile())
 
     cron.schedule('*/30 * * * *', () => cronSportsBookHourly())
     cron.schedule('*/30 * * * *', () => cronLoadMlbPlayer())
