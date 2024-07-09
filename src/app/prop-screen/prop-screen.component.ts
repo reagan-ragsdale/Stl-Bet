6a81d9f251d078185db01f7256b14c76b6a07f15
Reagan Ragsdale <rayrags10@gmail.com>
2024-07-09 12:42:42 -0500

    sda

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -2847,6 +2847,7 @@ export class PropScreenComponent implements OnInit {
   }
   public returnObj: any = {}
 public count = 0
+public arrayOfTeamBets: any[] = [];
   getTeamStats(team: DbGameBookData, teamName: string){
 
 try{
@@ -3235,9 +3236,23 @@ try{
     
     
     console.log(this.returnObj)
-    
+    this.arrayOfTeamBets.push(this.returnObj)
     return this.returnObj
 
+  }
+  public teamBestBets: any[] = []
+  //how to find best bets
+  //if either overall home/away or team win percentage is over a certain number then add it to it
+  //
+  getTeamBestBets(){
+    for(let bet of this.arrayOfTeamBets){
+      if(bet.totalGames != 0 && bet.totalGamesHomeAway != 0 && bet.totalGamesTeam != 0){
+        if(((bet.totalWins / bet.totalGames) > .7) || ((bet.totalWinsHomeAway / bet.totalGamesHomeAway) > .7) || ((bet.totalWinsTeam / bet.totalGamesTeam) > .7)){
+          this.teamBestBets.push(bet)
+        }
+      }
+      
+    }
   }
 
   loadFindHomeAwayFromGameId(gameId: string, teamName: string): string{
