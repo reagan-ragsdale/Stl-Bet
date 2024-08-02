9dc08db151bf1b329315e25826f180c5af63b7cd
Reagan Ragsdale <rayrags10@gmail.com>
2024-08-02 09:07:13 -0500

    sd

--- src/shared/Controllers/UsersController.ts
+++ src/shared/Controllers/UsersController.ts
@@ -2,7 +2,6 @@ import { Allow, BackendMethod, remult } from "remult"
 
 import { DbUsers, userRepo } from "../dbTasks/DbUsers";
 import { generate, verify } from 'password-hash'
-import { setSessionUser } from "../../server/server-session";
 
 export class UsersController {
 
@@ -10,11 +9,18 @@ export class UsersController {
   
     @BackendMethod({ allowed: true })
     static async signUp(username: string, password: string) {
-      if (!password) throw Error('Password is required')
-      const user = await userRepo.insert({
-        userName: username,
-        userPass: generate(password)
-      })
+        let users = await userRepo.find({where:{userName: username}})
+        if(users.length > 0){
+            throw Error('There is someone already with that usernamne')
+        }
+        else{
+            if (!password) throw Error('Password is required')
+                const user = await userRepo.insert({
+                  userName: username,
+                  userPass: generate(password)
+                })
+        }
+      
       //return setSessionUserBasedOnUser(user)
     }
   
