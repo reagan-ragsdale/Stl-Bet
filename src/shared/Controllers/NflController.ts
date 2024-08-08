28ec51e8ffc00b62bda98da8926f64fb06094274
Reagan Ragsdale <rayrags10@gmail.com>
2024-08-08 16:22:27 -0500

    asd

--- src/shared/Controllers/NflController.ts
+++ src/shared/Controllers/NflController.ts
@@ -28,5 +28,29 @@ export class NflController {
         await taskRepo.insert(playerStats)
     }
 
+    @BackendMethod({allowed:true})
+    static async nflGetPlayerGameStatsByPlayerId(playerId: number): Promise<DBNflPlayerGameStats[]>{
+        const taskRepo = remult.repo(DBNflPlayerGameStats)
+        return await taskRepo.find({where: {playerId: playerId}, orderBy: {gameDate: 'desc'}})
+    }
+
+    @BackendMethod({allowed:true})
+    static async nflGetPlayerGameStatsByPlayerIdAndSeason(playerId: number, season: number): Promise<DBNflPlayerGameStats[]>{
+        const taskRepo = remult.repo(DBNflPlayerGameStats)
+        return await taskRepo.find({where: {playerId: playerId, season: season}, orderBy: {gameDate: 'desc'}})
+    }
+
+    @BackendMethod({allowed:true})
+    static async nflGetAllPlayerGameStatsBySeason(season: number): Promise<DBNflPlayerGameStats[]>{
+        const taskRepo = remult.repo(DBNflPlayerGameStats)
+        return await taskRepo.find({where: {season: season}, orderBy: {gameDate: 'desc'}})
+    }
+
+    @BackendMethod({allowed:true})
+    static async nflGetAllPlayerGameStats(): Promise<DBNflPlayerGameStats[]>{
+        const taskRepo = remult.repo(DBNflPlayerGameStats)
+        return await taskRepo.find({orderBy: {gameDate: 'desc'}})
+    }
+
 
 }
