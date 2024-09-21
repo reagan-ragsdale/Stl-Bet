b40377cedbf82cb9a4fc6e820bcf8be0e69a336e
Reagan Ragsdale <rayrags10@gmail.com>
2024-09-21 13:30:47 -0500

    sd

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -473,8 +473,8 @@ export class PropScreenComponent implements OnInit {
       let teams = await TeamInfoController.getAllTeamInfo('NFL')
       let team1Info = teams.filter(e => e.teamNameFull == team1[0].teamName)
       let team2Info = teams.filter(e => e.teamNameFull == team2[0].teamName)
-      this.team1GameStats = await NflController.nflGetAllTeamGameStatsByNameAndSeason(team1Info[0].teamNameAbvr, 2023)
-      this.team2GameStats = await NflController.nflGetAllTeamGameStatsByNameAndSeason(team2Info[0].teamNameAbvr, 2023)
+      this.team1GameStats = await NflController.nflGetAllTeamGameStatsByNameAndSeason(team1Info[0].teamNameAbvr, 2024)
+      this.team2GameStats = await NflController.nflGetAllTeamGameStatsByNameAndSeason(team2Info[0].teamNameAbvr, 2024)
     }
     this.team1GameStatsReversed = JSON.parse(JSON.stringify(this.team1GameStats))
     this.team1GameStatsReversed = this.team1GameStatsReversed.reverse()
@@ -593,7 +593,7 @@ export class PropScreenComponent implements OnInit {
     else if(this.selectedSport == 'NFL'){
       let uniquePlayerNames = this.playerPropData.map(e => e.playerName).filter((value, index, array) => array.indexOf(value) === index)
       
-      this.playerStatsFinal = await NflController.nflGetAllPlayerGameStatsByPlayerNameAndSeason(uniquePlayerNames, 2023)
+      this.playerStatsFinal = await NflController.nflGetAllPlayerGameStatsByPlayerNameAndSeason(uniquePlayerNames, 2024)
     }
     
     //this.playerpropDataFinal is a 3 length array with 2 props for each player within it
