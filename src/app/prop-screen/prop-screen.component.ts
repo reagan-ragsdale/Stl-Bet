7e0dca08b4091f2266d1e01ca3a8d324374f45ca
Reagan Ragsdale <rayrags10@gmail.com>
2024-11-17 13:30:31 -0600

    asd

--- src/app/prop-screen/prop-screen.component.ts
+++ src/app/prop-screen/prop-screen.component.ts
@@ -2391,9 +2391,9 @@ export class PropScreenComponent implements OnInit {
     try {
       console.log('Incoming team name below')
       console.log(teamName)
-      let teamNameAbvr = this.allSportTeamInfo.filter(e => e.teamNameFull == teamName)
-      console.log(teamNameAbvr)
-      let teamNameAbvrFinal = teamNameAbvr[0].teamNameAbvr
+      let teamNameAbreviation = this.allSportTeamInfo.filter(e => e.teamNameFull == teamName)
+      console.log(teamNameAbreviation)
+      let teamNameAbvrFinal = teamNameAbreviation[0].teamNameAbvr
 
       let marketKey = team.marketKey
       let propType = this.getPropType(team.marketKey)
