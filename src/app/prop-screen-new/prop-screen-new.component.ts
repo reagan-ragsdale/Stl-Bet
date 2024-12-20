f03dbc83ff12c66397a09680fd766f9b9c9e3c30
Reagan Ragsdale <rayrags10@gmail.com>
2024-12-20 11:30:34 -0600

    bn

--- src/app/prop-screen-new/prop-screen-new.component.ts
+++ src/app/prop-screen-new/prop-screen-new.component.ts
@@ -55,6 +55,7 @@ export class PropScreenNewComponent implements OnInit {
   showSpinner: boolean = false;
   overUnderSlide: boolean = false;
   index: number = 0
+  moneyLineTableColumns: string[] = ["TeamAgainst", "Date", "Score"]
 
 
 
