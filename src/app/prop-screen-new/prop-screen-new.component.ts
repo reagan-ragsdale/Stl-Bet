29631deac130a3ab45f9df3fe5d215acfccfda80
Reagan Ragsdale <rayrags10@gmail.com>
2024-12-28 11:36:34 -0600

    asd

--- src/app/prop-screen-new/prop-screen-new.component.ts
+++ src/app/prop-screen-new/prop-screen-new.component.ts
@@ -237,6 +237,9 @@ export class PropScreenNewComponent implements OnInit, AfterViewInit {
     if(this.selectedProp[0].length > 1){
       this.selectedDisplayProp = this.selectedProp[this.index][this.overUnderSlide ? 1 : 0]
     }
+    else if(this.selectedDisplayProp[0].propType == 'altTotal'){
+      this.selectedDisplayProp = this.selectedProp[this.index]
+    }
     else{
       this.selectedDisplayProp = this.selectedProp[this.overUnderSlide ? 1 : 0]
     }
