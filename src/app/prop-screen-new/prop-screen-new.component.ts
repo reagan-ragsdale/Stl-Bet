f6447913ef7c7d302911115fcf95e6b2b4c88dd0
Reagan Ragsdale <rayrags10@gmail.com>
2024-12-28 11:53:42 -0600

    ads

--- src/app/prop-screen-new/prop-screen-new.component.ts
+++ src/app/prop-screen-new/prop-screen-new.component.ts
@@ -263,6 +263,8 @@ export class PropScreenNewComponent implements OnInit, AfterViewInit {
     else{
       this.selectedDisplayProp = this.selectedProp[this.index]
     }
+    console.log(this.index)
+    console.log(this.selectedDisplayProp)
     
   }
 
