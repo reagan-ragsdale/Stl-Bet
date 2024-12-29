de21226c383aa44e695a80d0d2794de286fb62a4
Reagan Ragsdale <rayrags10@gmail.com>
2024-12-29 12:08:52 -0600

    asd

--- src/app/prop-screen-new/prop-screen-new.component.ts
+++ src/app/prop-screen-new/prop-screen-new.component.ts
@@ -225,6 +225,7 @@ export class PropScreenNewComponent implements OnInit, AfterViewInit {
     }
     if(this.selectedPropType == 'Team Props'){
       this.selectedDisplayArray = this.teamPropFinnal
+      this.onPropClicked(this.selectedDisplayArray[0],0,0)
     }
     else if(this.selectedPropType == 'Player Props'){
       this.selectedDisplayArray = this.playerPropData
