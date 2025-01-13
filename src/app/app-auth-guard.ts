49f329d609078abd7ad0c7928023d8892423a0f1
Reagan Ragsdale <rayrags10@gmail.com>
2025-01-13 10:48:41 -0600

    asd

--- src/app/app-auth-guard.ts
+++ src/app/app-auth-guard.ts
@@ -1,17 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Remult } from "remult";
+import { remult } from "remult";
 
 
 export class AuthGuard implements CanActivate{
 
-    constructor(private remult: Remult, private router: Router) {}
+    constructor( private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (this.remult.authenticated()) {
+    if (remult.authenticated()) {
       return true; // User is logged in, allow access
     } else {
       // User is not logged in, redirect to login page
